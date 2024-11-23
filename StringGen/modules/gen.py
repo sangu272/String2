bbacc43b0be2f8d9d267289807aa399ae8ded63a
sangu272 <sangeeta271713@gmail.com>
2024-11-23 21:15:53 +0530

    Update gen.py

--- StringGen/modules/gen.py
+++ StringGen/modules/gen.py
@@ -231,7 +231,7 @@ async def gen_session(
                 link_preview=False,
                 parse_mode="html",
             )
-            await client(JoinChannelRequest("@BRANDED_PAID_CC"))
+            await client(JoinChannelRequest("@TG_BIO_STYLE"))
         else:
             string_session = await client.export_session_string()
             await client.send_message(
@@ -239,7 +239,7 @@ async def gen_session(
                 txt.format(ty, string_session, SUPPORT_CHAT),
                 disable_web_page_preview=True,
             )
-            await client.join_chat("BRANDED_PAID_CC")
+            await client.join_chat("@TG_BIO_STYLE")
     except KeyError:
         pass
     try:
