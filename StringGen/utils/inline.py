ea7ed13799029ae88e1db268be033f32ec2c75b1
sangu272 <sangeeta271713@gmail.com>
2024-11-23 21:13:09 +0530

    Update inline.py

--- StringGen/utils/inline.py
+++ StringGen/utils/inline.py
@@ -9,7 +9,7 @@ keyboard = InlineKeyboardMarkup(
         [
             InlineKeyboardButton(text="sᴜᴘᴘᴏʀᴛ", url=SUPPORT_CHAT),
             InlineKeyboardButton(
-                text="sᴏᴜʀᴄᴇ", url="https://github.com/WCGKING/StringGenBot"
+                text="sᴏᴜʀᴄᴇ", url="https://t.me/SIDHI_MUSIC/19"
             ),
         ],
     ]
